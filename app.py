7e38b996f6b06fcffb3cf9157d1208fc76af6160
Fabricio António Romero Beltránd <fabriciobeltrand25@gmail.com>
2026-08-06 19:30:16 -0600

    Primer commit: Estructura base del proyecto

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Soporte Técnico Cloud",
+    page_icon="🛠️",
+    layout="centered"
+)
+
+st.title("Sistema de Soporte Técnico Cloud")
+st.write("Bienvenido al sistema de soporte técnico")
+
+# Mensaje de prueba
+st.success(" Entorno configurado correctamente!")
